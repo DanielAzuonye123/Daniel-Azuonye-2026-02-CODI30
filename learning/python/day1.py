dfbc7ce260bbb77e6b68b0ff6da2c7eb83f92927
DanielAzuonye123 <danielazuonye36@gmail.com>
2026-04-15 10:37:43 -0500

    Create learning folder structure for Python and Java

---
+++ learning/python/day1.py
@@ -0,0 +1,29 @@
+print("=== Python Day 1 ===")
+
+# variables
+name = "Daniel"
+age = 16
+height = 175.26
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# input
+user_name = input("What is your name? ")
+print("Hello", user_name)
+
+# numbers
+num1 = 5
+num2 = 3
+
+print("Addition:", num1 + num2)
+print("Multiplication:", num1 * num2)
+
+# if statement
+guess = float(input("Guess a number: "))
+
+if guess == 7:
+    print("Correct!")
+else:
+    print("Wrong number")
