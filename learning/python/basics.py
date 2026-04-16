1d81e320db88f95f4b1e99cb4a969d438d7335ec
DanielAzuonye123 <danielazuonye36@gmail.com>
2026-04-16 10:32:26 -0500

    Create full learning tracking system

--- learning/python/basics.py
+++ learning/python/basics.py
@@ -9,3 +9,28 @@ print("Age:", age)
 user = input("Type your name: ")
 print("Hello", user)
 
+print("\n=== Day 2 ===")
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+# number comparison
+num = int(input("Enter a number: "))
+
+if num > 10:
+    print("Greater than 10")
+elif num == 10:
+    print("Exactly 10")
+else:
+    print("Less than 10")
+
+favorite = int(input("What is your favorite number? "))
+
+if favorite == 7:
+    print("lucky")
+else:
+    print("not lucky")
