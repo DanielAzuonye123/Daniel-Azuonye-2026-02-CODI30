66beb06b717bc82725b63b1941a7d558c225477a
DanielAzuonye123 <198103126+DanielAzuonye123@users.noreply.github.com>
2026-05-05 10:05:05 -0500

    Add additional examples showing difference between single and double quotes

--- quotations.sh
+++ quotations.sh
@@ -2,3 +2,11 @@
 # Quotes control how Bash reads text.
 # They affect whether variables expand and how spaces are handled.
 
+# More examples:
+
+echo "My name is $name"
+echo 'My name is $name'
+
+# Difference:
+# Double quotes → variable works
+# Single quotes → printed literally
