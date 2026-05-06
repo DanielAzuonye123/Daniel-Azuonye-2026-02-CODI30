8fc800309ee4f0810522f094d34045b5db8c80e7
DanielAzuonye123 <198103126+DanielAzuonye123@users.noreply.github.com>
2026-05-06 10:37:27 -0500

    added functions with explanation and examples

--- functions.sh
+++ functions.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# =========================
 # FUNCTIONS IN BASH
-# A function is a reusable block of code
-# It helps avoid repeating yourself
+# =========================
+# A function is a reusable block of code.
+# It helps avoid repeating yourself.
 
+# Simple function
 say_hello() {
-  echo "Hello World"
+    echo "Hello World"
 }
 
 say_hello
 
+# Function with parameter
 say_name() {
-  echo "Hello $1"
+    echo "Hello $1"
 }
 
 say_name "Daniel"
