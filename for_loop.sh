7c5e1058eedec4527e7b410345cc02fefd75b812
DanielAzuonye123 <198103126+DanielAzuonye123@users.noreply.github.com>
2026-05-06 10:41:40 -0500

    added for loop script with basic examples

---
+++ for_loop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# =========================
+# FOR LOOPS
+# =========================
+# Used to repeat actions a set number of times.
+
+# Loop through numbers
+for i in 1 2 3 4 5
+do
+    echo "Number: $i"
+done
+
+# Loop through words
+for name in Daniel Alex Jordan
+do
+    echo "Name: $name"
+done
